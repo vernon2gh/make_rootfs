e284c1723eae4014a83865be781fd6f3e733f0b3
Vernon Yang <vernon2gm@gmail.com>
2022-11-24 17:44:24 +0800

    Remove ubuntu_base directory and .config file

--- make_rootfs.sh
+++ make_rootfs.sh
@@ -3,11 +3,9 @@
 ROOTFS_NAME=rootfs
 ROOTFS_TYPE=ext4
 OUTPUT=out
-CONFIG=$OUTPUT/.config
 DOWNLOAD=$OUTPUT/download
-UBUNTU_BASE_DIR=$OUTPUT/ubuntu_base
 
-mkdir -p overlay $DOWNLOAD $UBUNTU_BASE_DIR
+mkdir -p overlay $DOWNLOAD
 echo "This is overlay root directory." > overlay/README
 
 function color_echo() {
@@ -31,25 +29,18 @@ if [[ $1 = "riscv64" && ! `which qemu-riscv64-static` ]]; then
 	sudo apt install qemu-user-static
 fi
 
-color_echo "Source default configure"
-if [ ! -f $CONFIG ]; then
-	echo "TARGET=" > $CONFIG
-fi
-
-source $CONFIG
-
 color_echo "Get target architecture and URL"
 case $1 in
 	"x86_64" | "")
-		TARGET_SET="x86_64" ## default target
+		TARGET="x86_64" ## default target
 		URL=http://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04-base-amd64.tar.gz
 		;;
 	"arm64")
-		TARGET_SET=$1
+		TARGET=$1
 		URL=http://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04-base-arm64.tar.gz
 		;;
 	"riscv64")
-		TARGET_SET=$1
+		TARGET=$1
 		URL=http://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04-base-riscv64.tar.gz
 		;;
 	*)
@@ -58,14 +49,6 @@ case $1 in
 		;;
 esac
 
-if [ "$TARGET_SET" = "$TARGET" ]; then
-	RECOVER="false"
-else
-	RECOVER="true"
-	sed -i "s/^TARGET=.*/TARGET=$TARGET_SET/" $CONFIG
-fi
-
-TARGET=$TARGET_SET
 ROOTFS=$OUTPUT/${ROOTFS_NAME}.${ROOTFS_TYPE}
 ROOTFS_TARGET_TYPE=$OUTPUT/${ROOTFS_NAME}_${TARGET}.${ROOTFS_TYPE}
 UBUNTU_BASE_PACKAGE=`basename $URL`
@@ -77,18 +60,19 @@ if [ ! -e $DOWNLOAD/$UBUNTU_BASE_PACKAGE ]; then
 fi
 
 if [ ! -e $ROOTFS_TARGET_TYPE ]; then
+	color_echo "Make root file system image"
+
+	dd if=/dev/zero of=$ROOTFS_TARGET_TYPE bs=1G count=1
+	mkfs.$ROOTFS_TYPE $ROOTFS_TARGET_TYPE
+	sudo mount $ROOTFS_TARGET_TYPE /mnt
+
 	color_echo "Decompression ubuntu base package"
 
-	sudo rm -fr $UBUNTU_BASE_DIR/*
-	tar -zxvf $DOWNLOAD/$UBUNTU_BASE_PACKAGE -C $UBUNTU_BASE_DIR
+	sudo tar -zxvf $DOWNLOAD/$UBUNTU_BASE_PACKAGE -C /mnt
 
 	touch ./install_software
-elif [ $RECOVER = "true" ]; then
-	color_echo "Recover ubuntu base package"
-
+else
 	sudo mount $ROOTFS_TARGET_TYPE /mnt
-	sudo cp -r /mnt/* $UBUNTU_BASE_DIR
-	sudo umount /mnt
 fi
 
 color_echo "Install software by apt"
@@ -97,19 +81,12 @@ file_timestamp=`stat -c %Y ./install_software`
 interval_timestamp=$[$now_timestamp - $file_timestamp]
 
 if [ $interval_timestamp -lt 180 ]; then ## 3 minutes
-	sudo /usr/bin/expect ./install_software $UBUNTU_BASE_DIR
+	sudo /usr/bin/expect ./install_software /mnt
 fi
 
 color_echo "Install software by overlay"
-cp -r overlay/* $UBUNTU_BASE_DIR
+sudo cp -r overlay/* /mnt
 
-color_echo "Make root file system image"
-ROOTFS_ZISE_MB=`sudo du -s -BM $UBUNTU_BASE_DIR | awk '{print $1}' | tr -cd "[0-9]"`
-
-dd if=/dev/zero of=$ROOTFS_TARGET_TYPE bs=100M count=`echo "($ROOTFS_ZISE_MB + 200)/100" | bc`
-mkfs.$ROOTFS_TYPE $ROOTFS_TARGET_TYPE
-sudo mount $ROOTFS_TARGET_TYPE /mnt
-sudo cp -r $UBUNTU_BASE_DIR/* /mnt
 sudo umount /mnt
 
 ln -sf `pwd`/$ROOTFS_TARGET_TYPE $ROOTFS

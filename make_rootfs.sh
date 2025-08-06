0fce8b4bfe3f0efd083be5056c2f39125c3734ff
Vernon Yang <vernon2gm@gmail.com>
2025-08-07 05:24:16 +0800

    support qcow2 images type
    Signed-off-by: Vernon Yang <vernon2gm@gmail.com>

--- make_rootfs.sh
+++ make_rootfs.sh
@@ -3,22 +3,23 @@
 ARCH="x86_64"    ## default target
 SOFTWARE=' '     ##
 MOUNT_POINT=/mnt ## default the mount point for the root filesystem
+QCOW2="false"
 
-ROOTFS_NAME=rootfs
-ROOTFS_TYPE=ext4
 ROOTDIR=`dirname $0`
 OVERLAY=$ROOTDIR/overlay
 OUTPUT=$ROOTDIR/out
 DOWNLOAD=$OUTPUT/download
 DEFAULT_SETTING=default_setting
 
+mkdir -p $OVERLAY $OUTPUT $DOWNLOAD
+
 function color_echo() {
 	echo -e "\e[32m[make_rootfs] $1\e[0m"
 }
 
 ## Parsing parameters
-SHORTOPTS="a:,i:,m:,h"
-LONGOPTS="arch:,install:,mount:,help"
+SHORTOPTS="a:,i:,m:,q,h"
+LONGOPTS="arch:,install:,mount:,qcow2,help"
 ARGS=$(getopt --options $SHORTOPTS	\
 	--longoptions $LONGOPTS -- "$@" )
 
@@ -40,6 +41,10 @@ do
 		mkdir -p $MOUNT_POINT
 		shift 2
 		;;
+	-q|--qcow2)
+		QCOW2="true"
+		shift
+		;;
 	-h|--help)
 		echo "Usage:"
 		echo "$0 <options>"
@@ -50,6 +55,7 @@ do
 		echo "-a, --arch x86_64/arm64/riscv64  Specify the architecture"
 		echo "-i, --install 'software'         Specify the software to be install"
 		echo "-m, --mount directory            Specifies the mount point for the root filesystem"
+		echo "-q, --qcow2                      Specifies create the root filesystem from qcow2 image"
 		echo "-h, --help                       Display this help"
 		exit
 		;;
@@ -69,67 +75,124 @@ if [ $ARCH = "x86_64" ]; then
 	ARCH=amd64
 fi
 
-color_echo "Install dependencies"
-if [ ! `which arch-chroot` ]; then
-	sudo apt install arch-install-scripts
-fi
+function rootfs_only()
+{
+	color_echo "Install dependencies"
+	if [ ! `which arch-chroot` ]; then
+		sudo apt install arch-install-scripts
+	fi
 
-if [ ! `which samba` ]; then
-	sudo apt install samba
-fi
+	if [ ! `which samba` ]; then
+		sudo apt install samba
+	fi
 
-if [[ $ARCH = "arm64" && ! `which qemu-aarch64-static` ]]; then
-	sudo apt install qemu-user-static
-fi
+	if [[ $ARCH = "arm64" && ! `which qemu-aarch64-static` ]]; then
+		sudo apt install qemu-user-static
+	fi
 
-if [[ $ARCH = "riscv64" && ! `which qemu-riscv64-static` ]]; then
-	sudo apt install qemu-user-static
-fi
+	if [[ $ARCH = "riscv64" && ! `which qemu-riscv64-static` ]]; then
+		sudo apt install qemu-user-static
+	fi
 
-color_echo "Get ubuntu-base URL"
-VERSION=24.04
-URL=https://cdimage.ubuntu.com/ubuntu-base/releases/${VERSION}/release/ubuntu-base-${VERSION}-base-${ARCH}.tar.gz
+	color_echo "Get ubuntu-base URL"
+	VERSION=24.04
+	URL=https://cdimage.ubuntu.com/ubuntu-base/releases/${VERSION}/release/ubuntu-base-${VERSION}-base-${ARCH}.tar.gz
 
-ROOTFS=$OUTPUT/${ROOTFS_NAME}.${ROOTFS_TYPE}
-ROOTFS_TARGET_TYPE=$OUTPUT/${ROOTFS_NAME}_${ARCH}.${ROOTFS_TYPE}
-UBUNTU_BASE_PACKAGE=`basename $URL`
+	ROOTFS_NAME=rootfs
+	ROOTFS_TYPE=ext4
+	ROOTFS=$OUTPUT/${ROOTFS_NAME}.${ROOTFS_TYPE}
+	ROOTFS_TARGET_TYPE=$OUTPUT/${ROOTFS_NAME}_${ARCH}.${ROOTFS_TYPE}
+	UBUNTU_BASE_PACKAGE=`basename $URL`
 
-mkdir -p $DOWNLOAD
+	if [ ! -e $DOWNLOAD/$UBUNTU_BASE_PACKAGE ]; then
+		color_echo "Download ubuntu base package"
+		wget $URL -P $DOWNLOAD
+	fi
 
-if [ ! -e $DOWNLOAD/$UBUNTU_BASE_PACKAGE ]; then
-	color_echo "Download ubuntu base package"
-	wget $URL -P $DOWNLOAD
-fi
+	if [ ! -e $ROOTFS_TARGET_TYPE ]; then
+		color_echo "Make root file system image"
+		dd if=/dev/zero of=$ROOTFS_TARGET_TYPE bs=1G count=10
+		mkfs.$ROOTFS_TYPE $ROOTFS_TARGET_TYPE
+		sudo mount $ROOTFS_TARGET_TYPE $MOUNT_POINT
 
-if [ ! -e $ROOTFS_TARGET_TYPE ]; then
-	color_echo "Make root file system image"
-	dd if=/dev/zero of=$ROOTFS_TARGET_TYPE bs=1G count=10
-	mkfs.$ROOTFS_TYPE $ROOTFS_TARGET_TYPE
-	sudo mount $ROOTFS_TARGET_TYPE $MOUNT_POINT
+		color_echo "Decompression ubuntu base package"
+		sudo tar -zxvf $DOWNLOAD/$UBUNTU_BASE_PACKAGE -C $MOUNT_POINT
 
-	color_echo "Decompression ubuntu base package"
-	sudo tar -zxvf $DOWNLOAD/$UBUNTU_BASE_PACKAGE -C $MOUNT_POINT
+		color_echo "Default setting"
+		sudo cp $ROOTDIR/$DEFAULT_SETTING $MOUNT_POINT
+		sudo arch-chroot $MOUNT_POINT /bin/bash /$DEFAULT_SETTING
+		sudo rm -fr $MOUNT_POINT/$DEFAULT_SETTING
+		sudo rm -fr $MOUNT_POINT/etc/resolv.conf
+		sudo ln -s /run/systemd/resolve/resolv.conf $MOUNT_POINT/etc/resolv.conf
+	else
+		sudo mount $ROOTFS_TARGET_TYPE $MOUNT_POINT
+	fi
 
-	color_echo "Default setting"
-	sudo cp $ROOTDIR/$DEFAULT_SETTING $MOUNT_POINT
-	sudo arch-chroot $MOUNT_POINT /bin/bash /$DEFAULT_SETTING
-	sudo rm -fr $MOUNT_POINT/$DEFAULT_SETTING
-	sudo rm -fr $MOUNT_POINT/etc/resolv.conf
-	sudo ln -s /run/systemd/resolve/resolv.conf $MOUNT_POINT/etc/resolv.conf
+	color_echo "Install software by apt"
+	if [ ! -z "$SOFTWARE" ]; then
+		sudo arch-chroot $MOUNT_POINT /bin/bash -c "apt install -y $SOFTWARE"
+	fi
+
+	color_echo "Install software by overlay"
+	mkdir -p $OVERLAY
+	sudo cp -r $OVERLAY/* $MOUNT_POINT
+
+	sudo umount $MOUNT_POINT
+
+	ln -sf `pwd`/$ROOTFS_TARGET_TYPE $ROOTFS
+}
+
+function rootfs_qcow2()
+{
+	color_echo "Install dependencies"
+	if [ ! `which qemu-img` ]; then
+		sudo dnf install qemu-img
+	fi
+
+	if [ ! `which virt-customize` ]; then
+		sudo dnf install guestfs-tools
+	fi
+
+	color_echo "Get ubuntu-base qcow2 URL"
+	VERSION=24.04
+	URL=https://cloud-images.ubuntu.com/releases/noble/release/ubuntu-${VERSION}-server-cloudimg-${ARCH}.img
+
+	ROOTFS_NAME=ubuntu
+	ROOTFS_TYPE=qcow2
+	ROOTFS=$OUTPUT/${ROOTFS_NAME}.${ROOTFS_TYPE}
+	ROOTFS_TARGET_TYPE=$OUTPUT/${ROOTFS_NAME}_${ARCH}.${ROOTFS_TYPE}
+	UBUNTU_BASE_PACKAGE=`basename $URL`
+
+	if [ ! -e $DOWNLOAD/$UBUNTU_BASE_PACKAGE ]; then
+		color_echo "Download ubuntu base qcow2 package"
+		wget $URL -P $DOWNLOAD
+	fi
+
+	if [ ! -e $ROOTFS_TARGET_TYPE ]; then
+		cp $DOWNLOAD/$UBUNTU_BASE_PACKAGE $ROOTFS_TARGET_TYPE
+		UBUNTU_QCOW2_IMG=$ROOTFS_TARGET_TYPE
+
+		qemu-img resize $UBUNTU_QCOW2_IMG +10G
+		virt-customize -a $UBUNTU_QCOW2_IMG				\
+			--root-password password:root				\
+			--run-command "growpart /dev/sda 1"			\
+			--run-command "resize2fs /dev/sda1"			\
+			--upload $DEFAULT_SETTING:/root/$DEFAULT_SETTING	\
+			--run-command "chmod +x /root/$DEFAULT_SETTING"		\
+			--run-command "/root/$DEFAULT_SETTING"			\
+			--delete /root/$DEFAULT_SETTING
+	fi
+
+	color_echo "Install software by apt"
+	if [ ! -z "$SOFTWARE" ]; then
+		virt-customize -a $UBUNTU_QCOW2_IMG --run-command "apt install -y $SOFTWARE"
+	fi
+
+	ln -sf `pwd`/$UBUNTU_QCOW2_IMG $ROOTFS
+}
+
+if [ $QCOW2 = "true" ]; then
+	rootfs_qcow2
 else
-	sudo mount $ROOTFS_TARGET_TYPE $MOUNT_POINT
+	rootfs_only
 fi
-
-color_echo "Install software by apt"
-if [ ! -z "$SOFTWARE" ]; then
-	sudo arch-chroot $MOUNT_POINT /bin/bash -c "apt install -y $SOFTWARE"
-fi
-
-
-color_echo "Install software by overlay"
-mkdir -p $OVERLAY
-sudo cp -r $OVERLAY/* $MOUNT_POINT
-
-sudo umount $MOUNT_POINT
-
-ln -sf `pwd`/$ROOTFS_TARGET_TYPE $ROOTFS
